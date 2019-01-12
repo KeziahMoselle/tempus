7f41bc27dc63bb430ba955ca2c9eb8a2b4ef393f
KeziahMoselle <keziah.moselle@gmail.com>
2019-01-12 23:19:14 +0100

    refactor: shorter conditions

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -4,12 +4,11 @@ export default ({ isCounting, start, pause, reset }) => {
   return (
     <div className="controls">
     <button onClick={reset} className="btn-danger"><i className="material-icons">stop</i></button>
-      { !isCounting &&
-        <button onClick={start} className="btn-primary"><i className="material-icons">play_arrow</i></button>
-      }
-      { isCounting &&
-        <button onClick={pause} className="btn-primary"><i className="material-icons">pause</i></button>
-      }
+    
+    
+    <button onClick={() => !isCounting ? start() : pause()} className="btn-primary">
+      <i className="material-icons">{ !isCounting ? 'play_arrow' : 'pause' }</i>
+    </button>
     </div>
   )
 }
