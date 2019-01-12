be4c79cba0bf1339c7e38c9ab7c03c77159189b1
KeziahMoselle <keziah.moselle@gmail.com>
2019-01-13 00:49:38 +0100

    feat: settings container (#7)

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default ({ isCounting, start, pause, reset }) => (
-  <footer>
+export default ({ isCounting, start, pause, reset }) => {
 
-    <div className="navbar">
-      <button onClick={reset}><i className="material-icons">stop</i></button>
-      <button onClick={() => !isCounting ? start() : pause()} className="overlap">
-      <i className="material-icons">{ !isCounting ? 'play_arrow' : 'pause' }</i>
-      </button>
-      <button><i className="material-icons">more_vert</i></button>
-    </div>
+  const [menu, setMenu] = useState(false)
+  useEffect(() => {
+    if (menu) {
+      document.querySelector('footer').style.height = '248px'
+    } else {
+      document.querySelector('footer').style.height = '56px'
+    }
+  }, [menu])
 
-  </footer>
-)
+  return (
+    <footer>
+
+      <div className="navbar">
+
+        <button onClick={reset}>
+          <i className="material-icons">stop</i>
+        </button>
+
+        <button onClick={() => !isCounting ? start() : pause()} className="overlap">
+          <i className="material-icons">{ !isCounting ? 'play_arrow' : 'pause' }</i>
+        </button>
+
+        <button onClick={() => setMenu(!menu)}>
+          <i className="material-icons">menu</i>
+        </button>
+
+      </div>
+
+      <div className="menu">
+        
+      </div>
+
+    </footer>
+  )
+}
