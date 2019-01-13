ec9ca1553682e053ea5c7bad063c56036fe7ff09
KeziahMoselle <keziah.moselle@gmail.com>
2019-01-13 01:56:35 +0100

    feat: input in menu (#7)

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -25,13 +25,22 @@ export default ({ isCounting, start, pause, reset }) => {
         </button>
 
         <button onClick={() => setMenu(!menu)}>
-          <i className="material-icons">menu</i>
+          <i className="material-icons">{ !menu ? 'menu' : 'close' }</i>
         </button>
 
       </div>
 
       <div className="menu">
-        
+        <ul>
+          <li>
+            <label for="work">Work</label>
+            <input id="work" type="number" placeholder="25" />
+          </li>
+          <li>
+            <label for="pause">Pause</label>
+            <input id="pause" type="number" placeholder="5" />
+          </li>
+        </ul>
       </div>
 
     </footer>
