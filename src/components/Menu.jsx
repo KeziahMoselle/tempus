4eeaf3e03faeb5ddd15b2d48d6cb5f9db4a4f8d0
KeziahMoselle <keziah.moselle@gmail.com>
2019-01-15 00:13:41 +0100

    feat: body of the card

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,14 +3,28 @@ import React from 'react'
 export default ({ toggleCards, cardsClass }) => (
   <div onClick={toggleCards} className={`cards ${cardsClass}`}>
 
-    <div onClick={(e) => e.stopPropagation()} className="card green">
-      <h2>Streak</h2>
-      <i className="material-icons">check_circle</i>
+    <div onClick={(e) => e.stopPropagation()} className="card">
+      <header className="green">
+        <h2>Streak</h2>
+        <i className="material-icons">check_circle</i>
+      </header>
+      <main className="extended">
+        <p>
+          lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsu
+        </p>
+      </main>
     </div>
 
-    <div onClick={(e) => e.stopPropagation()} className="card blue">
-      <h2>Statistics</h2>
-      <i className="material-icons">trending_up</i>
+    <div onClick={(e) => e.stopPropagation()} className="card">
+      <header className="blue">
+        <h2>Statistics</h2>
+        <i className="material-icons">trending_up</i>
+      </header>
+      <main>
+        <p>
+          lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem 
+        </p>
+      </main>
     </div>
 
   </div>
