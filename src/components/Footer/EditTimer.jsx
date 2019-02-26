b76fda7215a2cabfd3b056157fa43b3af6ae9f9b
KeziahMoselle <keziahmoselle@protonmail.com>
2019-02-26 22:44:51 +0100

    ui: improve ui/ux

--- src/components/Footer/EditTimer.jsx
+++ src/components/Footer/EditTimer.jsx
@@ -13,40 +13,52 @@ export default ({ setWork, setPause, resetTime, total, totalPause, setNumberOfCy
       <h3>Time</h3>
 
       <div className="field">
-        <label htmlFor="work">Work</label>
-        <input
-          onChange={(event) => setWork(event.target.value)}
-          id="work"
-          type="number"
-          min="1"
-          value={total / 60}
-        />
+        <label htmlFor="work">Work time</label>
+        <div className="has-suffix">
+          <input
+            onChange={(event) => setWork(event.target.value)}
+            id="work"
+            type="number"
+            min="1"
+            value={total / 60}
+          />
+
+          <div className="suffix">min</div>
+        </div>
       </div>
 
       <div className="field">
-        <label htmlFor="pause">Pause</label>
-        <input
-          onChange={(event) => setPause(event.target.value)}
-          id="pause"
-          type="number"
-          min="1"
-          value={totalPause / 60}
-        />
+        <label htmlFor="pause">Break time</label>
+        <div className="has-suffix">
+          <input
+            onChange={(event) => setPause(event.target.value)}
+            id="pause"
+            type="number"
+            min="1"
+            value={totalPause / 60}
+          />
+
+          <div className="suffix">min</div>
+        </div>
       </div>
     </div>
 
     <div className="card">
-      <h3>Cycle {parseInt(numberOfCycle, 10) === 0 ? '(disabled)' : ''}</h3>
+      <h3>Cycle</h3>
 
       <div className="field">
-        <label htmlFor="cycle">The number of cycle</label>
-        <input
-          onChange={(event) => setNumberOfCycle(event.target.value)}
-          id="cycle"
-          type="number"
-          min="0"
-          value={numberOfCycle}
-        />
+        <label htmlFor="cycle">Repeat</label>
+        <div className="has-suffix">
+          <input
+            onChange={(event) => setNumberOfCycle(event.target.value)}
+            id="cycle"
+            type="number"
+            min="0"
+            value={numberOfCycle}
+          />
+
+          <div className="suffix">cycles</div>
+        </div>
       </div>
     </div>
     
