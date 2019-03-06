4b008ca8d30411c693daa91747d86646bb288c42
KeziahMoselle <keziahmoselle@protonmail.com>
2019-03-06 14:35:55 +0100

    perf: enable react strict mode

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App.jsx'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'))
